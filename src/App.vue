a39041374ddd69071e39c8c2351bca0de4e8201f
unknown <vietanhtrinh@protonmail.com>
2019-10-24 21:26:27 +0200

    second commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <search @search="changeMovieResults"></search>
+    <movie-list :movies="movies"></movie-list>
+  </div>
+</template>
+
+<script>
+import Search from './components/Search'
+import MovieList from './components/MovieList'
+
+export default {
+  name: 'app',
+  components: {
+    Search,
+    MovieList
+  },
+  data () {
+    return {
+      movies: []
+    }
+  },
+  methods: {
+    changeMovieResults (results) {
+      this.movies = results
+    }
+  }
+}
+</script>
+
+<style>
+</style>
