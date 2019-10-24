c9fbe1f3b72b89ea46c5cd3a18e4f8c3f39bcf17
unknown <vietanhtrinh@protonmail.com>
2019-10-24 22:21:21 +0200

    finished 1st phase

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,12 +1,17 @@
 <template>
   <ul>
-  <li v-for="movie in movies" :key="movie.imdbID">{{ movie.Title }}</li>
+  <movie-list-item li v-for="movie in movies" :movie="movie" :key="movie.imdbID" />
 </ul>
 </template>
  
 <script>
+import MovieListItem from './MovieListItem'
+
 export default {
   name: 'movie-list',
+  components: {
+    MovieListItem
+  },
   props: {
     movies: {
       type: Array,
--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <input v-model="searchQuery" @keyup.enter="search"/>
+    <span v-if="error">{{ error }}</span>
   </div>
 </template>
 
@@ -11,7 +12,8 @@ export default {
   name: 'search',
   data () {
     return {
-      searchQuery: ''
+      searchQuery: '',
+      error: ''
     }
   },
   methods: {
@@ -20,8 +22,10 @@ export default {
       if (response.status === 200) {
         let data = response.data
         if (data.Response === 'True') {
+            this.error = ''
           return this.$emit('search', data.Search)
         }
+        this.error = data.Error
       }
     }
   }
